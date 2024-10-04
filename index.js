3de0ad021da62ab098f02fa381869e9a333e5b22
n4t5Uuu <olmedoalden4@gmail.com>
2024-10-04 09:51:51 +0800

    the blog banner and the navbar is now resized when it is in the size of the device

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-    const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('open');
-}
+// Select the hamburger and the navigation links
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+// Add event listener to toggle the 'show' class on click
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
